ffdef761425476af2468fbe6e25b01242ddbba24
Maydell <mats.stichel@gmail.com>
2015-10-13 12:26:19 +0200

    Waiting for anaphora code

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys
 import nltk
 
-fact_grammar = "FACT: {<LS|DT><JJ.*>*<NN.*>}"
+fact_grammar = """
+    FACT: {<LS|DT><JJ.*>*<NN.*><VB.*>*}
+    """
 
 if len(sys.argv) < 2:
     print "Usage: python main.py <filename>"
@@ -10,3 +12,12 @@ if len(sys.argv) < 2:
 # Read the filename
 filename = sys.argv[1]
 file object = open(filename, "r")
+
+text = object.read()
+
+# Call anaphora resolution
+
+cp = nltk.RegexpParser(fact_grammar)
+result = cp.parse(tokenized)
+print result
+result.draw()
