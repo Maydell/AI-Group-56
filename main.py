ae89d5ebe79b3544ead34babe2323ec93cb548b7
Maydell <mats.stichel@gmail.com>
2015-10-15 14:43:02 +0200

    Added classes for facts 3 and 4

--- main.py
+++ main.py
@@ -61,6 +61,42 @@ class HasPropertyWithValueRecord(HasPropertyRecord):
     def __str__(self):
         return "HasPropertyWithValue<" + self.who + " : " + self.what_property + " : " + str(self.how_much) + "(" + self.units + ")>"
 
+class RelationToEntity(object):
+    """A database record meaning someone has a relation to someone else"""
+    def __init__(self, who, verb, how, whom):
+        super(RelationToEntity, self).__init__()
+        self.who = who
+        self.verb = verb
+        self.how = how
+        self.whom = who
+
+    def __str__(self):
+        return "Fact<" + self.who + " : " + self.verb + " "  + ", ".join(how) + " " + self.whom + ">"
+
+class RelationToNoun:
+    """A database record meaning someone has a relation to something"""
+    def __init__(self, who, verb, dt, what):
+        super(RelationToNoun, self).__init__()
+        self.who = who
+        self.verb = verb
+        self.dt = dt
+        self.what = what
+
+    def __str__(self):
+        return "Fact<" + self.who + " : " + self.verb + " " + self.dt + " " + self.what + ">"
+
+class RelationToEntities:
+    """A database record meaning someone has a relation to multiple entities"""
+    def __init__(self, who, verb, num, whom):
+        super(RelationToEntities, self).__init__()
+        self.who = who
+        self.verb = verb
+        self.num = num
+        self.whom = whom
+
+    def __str__(self):
+        return "Fact<" + self.who + " : " + self.verb + " " + self.num + " " + self.whom + ">"
+
 for result in results:
     label = result[0].label()
     noun = result[0][0]
@@ -95,3 +131,9 @@ for result in results:
             print("Couldn't udnerstand!")
             print(what)
 
+
+    if label == "FACT3":
+        who = result[0][0][0]
+        verb = result[0][1][0]
+        for (word, postag) in result[0][3:-1]:
+            if postag == "JJ":
