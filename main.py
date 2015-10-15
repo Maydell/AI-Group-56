1df267af69b04695178a8a4114d8f414c48e2353
Joel <jopt@me.com>
2015-10-15 14:08:03 +0200

    HasProperty record sturct and basic parsing

--- main.py
+++ main.py
@@ -42,14 +42,31 @@ for sentence in pos_tags:
 
 nouns = []
 
+class HasPropertyRecord(object):
+    """A database record meaning someone has some property"""
+    def __init__(self, who, what_property):
+        super(HasPropertyRecord, self).__init__()
+        self.who = who
+        self.what_property = what_property
+
+    def __str__(self):
+        return "Fact<" + self.who + " : " + self.what_property + ">"
+
 for result in results:
     label = result[0].label()
     noun = result[0][0]
-    print(noun[0])
+    # print(noun[0])
 
     if label == "FACT1":
-        print(result[0][1][0])
-        print(result[0][2][0])
-        for part in result[0][3:]:
-            if part[1] == "JJ":
-                print(part[0])
+        who = result[0][0][0]
+        verb = result[0][1][0]
+        if verb == "is":
+            print("Found a fact where someone is something!")
+            for (word, postag) in result[0][3:]:
+                if postag == "JJ": # Just in case
+                    record = HasPropertyRecord(who, word)
+                    print(record)
+        else:
+            print("Couldn't udnerstand!")
+            print(what)
+
