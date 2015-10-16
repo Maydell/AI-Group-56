56ead1298535e8996c6a2bcb0cd907c107ea95b2
Mats Stichel <mats.stichel@gmail.com>
2015-10-16 23:20:38 +0200

    Now prints contradictions and facts prettier

--- analysis.py
+++ analysis.py
@@ -4,7 +4,7 @@ import text2int
 def analyze(parsed_sent):
 
     # print(parsed_sent)
-    results = []
+    results = {"FACT1": [], "FACT2": [], "FACT3": [], "FACT6": []}
 
     # Not all attributes will have labels; we want to be resilient
     try:
@@ -16,11 +16,11 @@ def analyze(parsed_sent):
         who = parsed_sent[0][0][0]
         verb = parsed_sent[0][1][0]
         if verb == "is":
-            for (word, postag) in parsed_sent[0][3:]:
+            for (word, postag) in parsed_sent[0][2:]:
                 if postag == "JJ": # Just in case
                     record = model.Property(who, word)
-                    results.append(record)
-    
+                    results["FACT1"].append(record)
+
     if label == "FACT2":
         who = parsed_sent[0][0][0]
         verb = parsed_sent[0][1][0]
@@ -34,7 +34,7 @@ def analyze(parsed_sent):
         except ValueError:
             f = text2int.convert(cardinal)
         record = model.QuantifiedProperty(who, adj, f, unit)
-        results.append(record)
+        results["FACT2"].append(record)
 
     if label == "FACT3":
         who = parsed_sent[0][0][0]
@@ -42,7 +42,27 @@ def analyze(parsed_sent):
         what = parsed_sent[0][-2][0]
         adjs = [word for (word,postag) in parsed_sent[0][2:-2] if postag == "JJ"]
         record = model.QualifiedRelation(who, verb, what, adjs)
-        results.append(record)
+        results["FACT3"].append(record)
+
+    if label == "FACT4":
+        who = parsed_sent[0][0][0]
+        verb = parsed_sent[0][1][0]
+        dt = None
+        adj = None
+
+        if len(parsed_sent[0] == 5):
+            dt = parsed_sent[0][2][0]
+            adj = parsed_sent[0][3][0]
+            whom = parsed_sent[0][4][0]
+        elif len(parsed_sent[0] == 4):
+            if parsed_sent[0][2][1] == "DT":
+                dt = parsed_sent[0][2][0]
+            elif parsed_sent[0][2][1].startswith("JJ"):
+                adj = parsed_sent[0][2][0]
+            whom = parsed_sent[0][3][0]
+        else:
+            whom = parsed_sent[0][3][0]
+
 
     if label == "FACT6":
         who = " ".join(w for (w, p) in parsed_sent[0][0])
@@ -57,6 +77,6 @@ def analyze(parsed_sent):
                     how.append(l[0])
         how = " ".join(how)
         record = model.QualifiedProperty(who, verbs_property, how)
-        results.append(record)
-        
+        results["FACT6"].append(record)
+
     return results
--- main.py
+++ main.py
@@ -10,6 +10,9 @@ import model
 # FACT4: Stephan killed a cat. Stephan killed Greg. Stephan is a man.
 # FACT5: Stephan is dying.
 
+contAdj = {"tall": ["short", "tiny"], "short": ["tall", "big"], "happy": ["sad", "angry"], "sad": ["happy"]}
+contVerb = {"likes": ["dislikes", "hates"], "hates": ["likes", "loves"], "dislikes": ["likes", "loves"]}
+
 grammar1 = """
     FACT3: {<NN.*><VB.*>(<JJ.*>(<CC|,><JJ.*>)*)?<NN.*><\.>}
     FACT1: {<NN.*><VB.*><JJ.*>(<CC|,>?<JJ.*>)*}
@@ -32,7 +35,7 @@ sents = []
 if sys.argv[1] == "brown":
     sents = nltk.corpus.brown.sents()
 elif sys.argv[1] == "reuters":
-    sents = nltk.corpus.reuters.sents() 
+    sents = nltk.corpus.reuters.sents()
 else:
     # Read the filename
     filename = sys.argv[1]
@@ -48,13 +51,44 @@ else:
 
 i = 0
 parsers = [nltk.RegexpParser(grammar1), nltk.RegexpParser(grammar2)]
+results = {"FACT1": [], "FACT2": [], "FACT3": [], "FACT6": []}
 for sent in sents:
     tagged_sent = nltk.pos_tag(sent)
     for parser in parsers:
         parsed_sent = parser.parse(tagged_sent)
-        results = analyze(parsed_sent)
-        if len(results) > 0:
-            for result in results:
-                i += 1
-                print(i, end=": ")
-                print(result)
+        results2 = analyze(parsed_sent)
+        for result in results2["FACT1"]:
+            results["FACT1"].append(result)
+        for result in results2["FACT3"]:
+            results["FACT3"].append(result)
+
+for result in results["FACT1"]:
+    conts = contAdj[result.what_property]
+    for other in results["FACT1"]:
+        for cont in conts:
+            if other.what_property == cont: # We have a contradiction
+                print(str(result.who) + " cannot be " + str(result.what_property) + " and " + other.what_property)
+
+for result in results["FACT3"]:
+    conts = contVerb[result.what_relation]
+    for other in results["FACT3"]:
+        if other.what_entity == result.what_entity: # Relation to same entity
+            for cont in conts:
+                if other.what_relation == cont:
+                    print(str(result.who) + " cannot " + str(result.what_relation) + " and " + str(other.what_relation) + " " + result.what_entity + " at the same time")
+
+print("FACT1")
+for result in results["FACT1"]:
+    print(result)
+
+print("FACT2")
+for result in results["FACT2"]:
+    print(result)
+
+print("FACT3")
+for result in results["FACT3"]:
+    print(result)
+
+print("FACT6")
+for result in results["FACT6"]:
+    print(result)
